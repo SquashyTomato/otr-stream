4528ae938b7f145cdf3e667fbc2b7fefaa08701f
Adam <18370423+SquashyTomato@users.noreply.github.com>
2025-07-30 23:06:37 +0100

    Adjusted Vite Build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 // Import Modules
 import { defineConfig } from 'vite';
-import path from 'node:path'
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // Vite Config (https://vite.dev/config)
 export default defineConfig({
-    root: path.resolve(__dirname, 'src/web'),
     build: {
-        outDir: '../dist/web',
+        outDir: './dist/web',
     },
     plugins: [
         react(),
