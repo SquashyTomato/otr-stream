58d1377336669f6a0bd3d06a321229fa8b9164a6
Adam <18370423+SquashyTomato@users.noreply.github.com>
2025-08-02 19:27:14 +0100

    Fixed Gameplay Size

--- src/views/components/GameplayBox.jsx
+++ src/views/components/GameplayBox.jsx
@@ -9,20 +9,20 @@ export default function Component({ number, color, data }) {
 
     return (
         <div className={clsx(
-            'w-[400px] h-[300px] outline-4 ',
+            'w-[400px] h-[324px] outline-4 ',
             {
-                'outline-tourney-red': color === 'red',
-                'outline-tourney-blue': color === 'blue',
+                'bg-green-400 outline-tourney-red': color === 'red',
+                'bg-green-400 outline-tourney-blue': color === 'blue',
             }
         )}>
             <div className={clsx(
-                'top-0 w-full h-4 flex items-center px-2',
+                'top-0 w-full h-[24px] flex items-center px-2',
                 {
                     'bg-tourney-red': color === 'red',
                     'bg-tourney-blue': color === 'blue',
                 }
             )}>
-                <h3 className="text-white text-left font-semibold">{isSpectating ? currentClient.spectating.name : 'Open'}</h3>
+                <h3 className="text-white text-2xl text-left font-semibold">{isSpectating ? currentClient.spectating.name : 'Open'}</h3>
             </div>
         </div>
     );
